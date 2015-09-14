07707243b42d37f24d2addc6693a070aa51713e0
mattylaug <mattsll9559@hotmail.com>
2015-09-14 12:15:01 +1000

    Revert "Revert "Revert "Initial commit of sprint 1."""
    This reverts commit 9a6011d571415e15d31d178994bba3f9c61474d6.

--- SPRINT1/database_connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con=mysql_connect("localhost", "root", "");
-
-if(!$con)
-    die("error in data base conection");
-
-mysql_select_db('audio');
-
-?>
--- SPRINT1/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	unset($_SESSION['name']);
-	unset($_SESSION['id']);
-	unset($_SESSION['status']);
-	session_destroy();
-	header("Location:index.php");
-?>
