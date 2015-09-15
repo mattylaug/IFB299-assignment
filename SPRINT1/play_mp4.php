7235dca1fc984f623d289b555270efe7782c2249
mattylaug <mattsll9559@hotmail.com>
2015-09-15 16:21:03 +1000

    Added video playing functionality to the UI.

--- SPRINT1/play_mp4.php
+++ SPRINT1/play_mp4.php
@@ -32,7 +32,50 @@ $id=$_GET['id'];
 
 </style>
 
+<script type="text/javascript">
 
+$(document).ready(function(){
+
+    /* Get iframe src attribute value i.e. YouTube video url
+
+    and store it in a variable */
+
+    var url = $(".play_video").attr('src');
+
+   // alert(url);
+
+    /* Assign empty url value to the iframe src attribute when
+
+    modal hide, which stop the video playing */
+
+    $("#myModal").on('hide.bs.modal', function(){
+
+        $("#cartoonVideo").attr('src', '');
+
+    });
+//     $("#myModal").on('hidden.bs.modal', function (e) {
+ 
+//  $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
+// });
+
+    
+
+    /* Assign the initially stored url back to the iframe src
+
+    attribute when modal is displayed again */
+    
+        $("#myModal").on('show.bs.modal', function(){
+
+        $("#cartoonVideo").attr('src', url);
+
+    });
+
+});
+
+    
+    
+
+</script>
 
 </head>
 
